ef5d9dea409c8a56676d0016edb4bffbea77eddd
程鵬 <chengandpeng@gmail.com>
2018-05-24 14:56:44 +0800

    update

--- ethereum/contracts/quiz.sol
+++ ethereum/contracts/quiz.sol
@@ -7,6 +7,7 @@ contract Quiz {
     string[2] public titles;
     uint256[2] public balances;
     mapping(address => uint256)[2] public playersMap;
+    uint8 public winSide;
     
     constructor(string _title1, string _title2) public {
         manager = msg.sender;
@@ -30,21 +31,39 @@ contract Quiz {
     }
     
     function pickWinner(uint8 side) public restricted(side) {
-        
+        uint totlaBalance = address(this).balance;
         for (uint i = 0; i < players.length; i++) {
             uint _balance = playersMap[side][players[i]];
             if (_balance > 0) {
-                uint rate = _balance / balances[side];
-                players[i].transfer(address(this).balance * rate );
+                uint v = totlaBalance * _balance / balances[side];
+                players[i].transfer(v);
+                playersMap[side][players[i]] = 0;
             }
         }
         
-        reset();
+        manager.transfer(address(this).balance);
+        winSide = side;
+        complete = true;
+    }
+    
+    function refund() public {
+        require(msg.sender == manager);
+        for (uint i = 0; i < 2; i++) {
+            for (uint j = 0; j < players.length; j++) {
+                uint _balance = playersMap[i][players[j]];
+                if (_balance > 0) {
+                    players[j].transfer(_balance);
+                    playersMap[i][players[j]] = 0;
+                }
+            }
+        }
     }
     
     function reset() public {
-        complete = true;
+        complete = false;
         players = new address[](0);
+        balances = [0, 0];
+        titles = ["", ""];
     }
     
     modifier restricted(uint8 side) {
